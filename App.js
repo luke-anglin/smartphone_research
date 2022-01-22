e1736c85d3cf990aec4799c5f2db9965037c77ea
Luke Anglin <lukeanglin@gmail.com>
2022-01-22 17:46:09 -0500

    Added feature to save file to device

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
+import * as FileSystem from "expo-file-system";
 
 export default function App() {
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Type here to send a text file to EC2!"
+        onChangeText={(newText) => {
+          setText(newText);
+          FileSystem.writeAsStringAsync(
+            FileSystem.documentDirectory + "test.txt",
+            text
+          )
+            .then(console.log("worked"));
+        }}
+        defaultValue={text}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
